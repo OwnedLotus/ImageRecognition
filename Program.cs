734396e3a50a84d7ba557427b24072a0acb10508
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-06-16 21:09:17 -0400

    add image file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ImageRecognition.models;
+
+var temp = new Image("data/text.txt");
+
+temp.TestDisplayData();
+
+var input = new float[] { 1.2f, 5.1f, 2.1f };
+var weight = new float[] { 3.1f, 2.1f, 8.7f };
+var bias = 3;
+
+var output = input[0] * weight[0] + input[1] * weight[1]
+        + input[2] * weight[2] + bias;
+
+Console.WriteLine(output);
---
+++ models/Image.cs
@@ -0,0 +1,56 @@
+using System.Text;
+using System.Numerics;
+
+
+
+namespace ImageRecognition.models;
+
+class Image
+{
+    private Guid id = Guid.NewGuid();
+    private string? imageHandle;
+    private Vector<byte> imageData;
+
+
+    public Image(string? handle)
+    {
+        imageHandle = handle;
+        byte[] data;
+
+        try
+        {
+            using (FileStream fsIn = new FileStream(imageHandle,
+                        FileMode.Open, FileAccess.Read))
+            {
+                var numBytesToRead = (int)fsIn.Length;
+                data = new byte[numBytesToRead];
+
+                var numBytesRed = 0;
+                while (numBytesToRead > 0)
+                {
+                    var n = fsIn.Read(data, numBytesRed, numBytesToRead);
+
+                    if (n == 0)
+                        break;
+
+                    numBytesRed += n;
+                    numBytesToRead -= n;
+                }
+                numBytesToRead = data.Length;
+            }
+
+            imageData = new Vector<byte>(data);
+        }
+        catch (FileNotFoundException ioEx)
+        {
+            Console.WriteLine(ioEx.Message);
+        }
+
+    }
+
+
+    public void TestDisplayData()
+    {
+        Console.WriteLine(imageData);
+    }
+}
